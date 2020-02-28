529d0d192e9aba91c8a9d44d23addf11fbcc655e
prabutdr <prabutdr@gmail.com>
2020-02-28 23:23:58 +0530

    Directives - started

--- hello-world/src/app/app.component.ts
+++ hello-world/src/app/app.component.ts
@@ -14,7 +14,20 @@ export class AppComponent {
     isFavorite: true
   }
 
+  courses = ['Course1', 'Course2', 'Course3', 'Course4'];
+
+  viewMode = 'some';
+
   onFavoriteChange(eventData: FavoriteChangeEventData) {
     console.log("Favorite changed", eventData);
   }
+
+  onAdd() {
+    this.courses.push('Course 10');
+  }
+
+  onRemove(course) {
+    const index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+  }  
 }
