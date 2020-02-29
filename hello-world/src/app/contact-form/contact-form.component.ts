444a3cce4d6226e12d8f2ed1a25034be65c03bb9
prabutdr <prabutdr@gmail.com>
2020-02-29 15:48:13 +0530

    Template driven forms - completed

--- hello-world/src/app/contact-form/contact-form.component.ts
+++ hello-world/src/app/contact-form/contact-form.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-contact-form',
+  selector: 'contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent implements OnInit {
+  contactMethods = [
+    { id: 1, name: 'Email' },
+    { id: 2, name: 'Phone' },
+    { id: 3, name: 'Swift' },
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  log(x) {
+    console.log(x);
+  }
+
+  submit(f) {
+    console.log(f);
+  }
 }
